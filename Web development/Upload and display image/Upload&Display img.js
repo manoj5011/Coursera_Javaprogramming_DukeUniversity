49120034b1043ad9e47d433b24197270e71b49ea
manoj5011 <saisiddarthmanoj@gmail.com>
2021-08-17 18:10:33 +0530

    Added a feature to convert image to greyscale image.

--- Web development/Upload and display image/Upload&Display img.js
+++ Web development/Upload and display image/Upload&Display img.js
@@ -1,6 +1,19 @@
+let orgImage;
+let greyImage
 function upload(){
 let canvas=document.getElementById("can");
 let fileInput=document.getElementById("imageUpload");
-let image=new SimpleImage(fileInput);
-image.drawTo(canvas);
+orgImage=new SimpleImage(fileInput);
+greyImage=new SimpleImage(fileInput);
+orgImage.drawTo(canvas);
+}
+function makeGrey(){
+for(let pixel of greyImage.values()){
+    let avg=(pixel.getRed()+pixel.getGreen()+pixel.getBlue())/3;
+    pixel.setRed(avg);
+    pixel.setBlue(avg);
+    pixel.setGreen(avg);
+}
+let canvas2=document.getElementById("can2");
+greyImage.drawTo(canvas2);
 }
